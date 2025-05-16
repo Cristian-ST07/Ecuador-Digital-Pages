ae4981796f8155b1b4e14d90e5a2763282abdc6d
Cristian Omar <Cristian_omar7@hotmail.es>
2025-05-16 15:50:50 -0500

    Versión estática del sitio

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-116.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-tzpuxaen' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-d688ba46' );
+});
+ });
